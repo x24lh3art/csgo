9e6433ce9980f218ce77f4ee2ca07918b01fd862
x24lh3art <59681079+x24lh3art@users.noreply.github.com>
2020-01-09 12:22:11 +0545

    Create process.py

---
+++ process.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import os
+import time
+
+useragent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-popup-blocking")
+chrome_options.add_argument("--allow-popups-during-page-unload")
+chrome_options.add_argument("--disable-background-timer-throttling")
+chrome_options.add_argument(f'user-agent={useragent}')
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+
+driver.get("https://z1process.blogspot.com/")
+print("Opened the web page.")
+driver.find_element_by_link_text("First Try").click()
+time.sleep(5)
+driver.find_element_by_link_text("website").click()
+print("Processing")
+time.sleep(1500)
+
+driver.quit()
+
+print("Did it.")
